406b2ac75418ef760d8756083345d6c7fbe34fe1
Richard Jennings <richardjennings@gmail.com>
2024-05-02 10:46:08 +0100

    sha

--- totp.rb
+++ totp.rb
@@ -2,7 +2,7 @@ class Totp < Formula
   desc "totp"
   homepage "https://github.com/richardjennings/totp"
   url "https://github.com/richardjennings/totp/releases/download/v0.0.1/totp_0.0.1_darwin_arm64.tar.gz"
-  sha256 "581680d0b91d9809c8981d4a8cb5ce78db20a2e255d9cf67aa7f96874e1d3e0e"
+  sha256 "425b02265ed5a56823e393f9d1aa987f29dfcdcbf5be66f8ac6ff851ae652d9c"
   version "0.0.1"
 
   def install
