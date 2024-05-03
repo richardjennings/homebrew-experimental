c4fe31ddfd0fb67dfcee1634efe92d9f14f6ef12
Richard Jennings <richardjennings@gmail.com>
2024-05-03 08:52:06 +0100

    sha

--- precrypt.rb
+++ precrypt.rb
@@ -2,7 +2,7 @@ class Precrypt < Formula
   desc "precrypt"
   homepage "https://github.com/richardjennings/precrypt"
   url "https://github.com/richardjennings/precrypt/releases/download/v0.0.2/precrypt_0.0.2_darwin_arm64.tar.gz"
-  sha256 ""
+  sha256 "903e5714bc7082369632000e72ce0d037d39901035c3db4bff02db3eaf09862d"
   version "0.0.2"
 
   def install
