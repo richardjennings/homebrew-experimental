80376dbfdc9ef52dde782bb2dcab7f100d78b792
Richard Jennings <richardjennings@gmail.com>
2024-05-03 08:51:22 +0100

    precrypt v0.0.2

--- precrypt.rb
+++ precrypt.rb
@@ -1,9 +1,9 @@
 class Precrypt < Formula
   desc "precrypt"
   homepage "https://github.com/richardjennings/precrypt"
-  url "https://github.com/richardjennings/precrypt/releases/download/v0.0.1/precrypt_0.0.1_darwin_arm64.tar.gz"
-  sha256 "93095966c03c854b87127baa248c7ca1da8d17465a04b159bcd075943b545ac7"
-  version "0.0.1"
+  url "https://github.com/richardjennings/precrypt/releases/download/v0.0.2/precrypt_0.0.2_darwin_arm64.tar.gz"
+  sha256 ""
+  version "0.0.2"
 
   def install
     bin.install "precrypt"
